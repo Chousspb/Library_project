bfde82b30154d4d3dbb5b68eff6bf2616127cb76
Chousspb <123@mail.com>
2023-11-24 12:39:12 +0300

    pep8 commit

--- library/views.py
+++ library/views.py
@@ -13,9 +13,11 @@ class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
 
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = CustomUserSerializer
