e6e3f5ae243c504ad3e5efa6bdb75f86fcf42d66
Chousspb <123@mail.com>
2023-11-24 12:29:56 +0300

    сделал bugfix

--- Library_project/urls.py
+++ Library_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: redirect('admin/')),
